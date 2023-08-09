6af9ee2b2855c2e6da89bb58c72ff09303900b54
Dev Phahla <dev.phahla@gmail.com>
2023-08-09 16:31:10 +0200

    added appspec.ym

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def health():
-    return make_response(json.dumps({"status": "OkK"}))
+    return make_response(json.dumps({"status": "Yes"}))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="3000")
